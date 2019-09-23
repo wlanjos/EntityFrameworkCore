67ceda2624542e9b6cfed2373b7c2b028ca921f6
Washington Anjos <wlanjos@hotmail.com>
2019-09-22 21:49:58 -0300

    Commit

---
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+        }
+
+        internal object GetInfraestructure<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paoFrances = new Produto()
+            {
+                Nome = "Pão Frances",
+                PrecoUnitario = 0.40,
+                Unidade = "Unidade",
+                Categoria = "Padaria"
+
+            };
+            var compra = new Compra();
+            compra.Quantidade = 6;
+            compra.Produto = paoFrances;
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+
+            using (var contexto = new LojaContext())
+            {
+
+                contexto.Compras.Add(compra);
+                contexto.SaveChanges();
+            }
+
+        }
+
+    }
+}
+
